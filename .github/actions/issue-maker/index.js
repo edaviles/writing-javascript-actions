3be51af77e63ff000f93904930da99efc6312728
Omar Aviles <edaviles@live.com>
2020-06-16 12:23:34 -0500

    Changing Github vs GitHub

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,14 +7,14 @@ async function run() {
       const jokeBody = core.getInput("joke");
       const token = core.getInput("repo-token");
   
-      const octokit = new github.Github(token);
+      const octokit = new github.GitHub(token);
   
-      //const newIssue = await octokit.issues.create({
-      //    repo: github.context.repo.repo,
-      //    owner: github.context.repo.owner,
-      //    title: issueTitle,
-      //    body: jokeBody
-      //});
+      const newIssue = await octokit.issues.create({
+          repo: github.context.repo.repo,
+          owner: github.context.repo.owner,
+          title: issueTitle,
+          body: jokeBody
+      });
     } catch (err) {
         core.setFailed(err.message);
     }
